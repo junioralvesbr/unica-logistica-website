b64f4a270779f3b02a1d239f88c434842bb1dc3e
Junior Alves <jrnalves@gmail.com>
2024-09-17 17:43:02 -0300

    Serviços: Criado o desktop de serviços

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+
+const FlipCard = plugin(function ({ addUtilities }) {
+  addUtilities({
+    ".my-rotate-y-180": {
+      transform: "rotateY(180deg)",
+    },
+    ".preserve-3d": {
+      transformStyle: "preserve-3d",
+    },
+    ".perspective": {
+      perspective: "1000px",
+    },
+    ".backface-hidden": {
+      backfaceVisibility: "hidden",
+    },
+  });
+});
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
@@ -12,8 +31,8 @@ module.exports = {
         secondary: "#6c3d70",
         third: "#ffde14",
         fourth: "#ffcd1c"
-      },
+      }
     },
   },
-  plugins: [],
+  plugins: [FlipCard],
 }
