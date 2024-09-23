8e0d98b9a5ff7880a73ccbb8e305d1dbd114be59
Junior Alves <jrnalves@gmail.com>
2024-09-23 19:56:13 -0300

    deploy da primeira versão do site (#4)
    * navnar: ajustes do tamanho dos testos da navbar
    
    * Serviços: Criado o desktop de serviços
    
    * services: refazendo a section para que seja responsivel
    
    * services section: Recriado a seção de serviço com responsividade
    
    * Metologia: Concluido a seção de metodologia e ajustado a seção nossos serviços
    
    * especialista: Criado a seção de especialista
    
    * Especialista: Terminado a seção de especialista
    
    * Caixas: Finalizado a seção das caixas
    
    * Especialista: ajuste de padding bottom
    
    * depoimento: Criado a seção de depoimento
    
    * Ajustado sequencia do tailwind
    
    * ajustes no carrocel do depoimento
    
    * depoimentos: Adicioado autoplau no carrocel do depoimento
    
    * ecosistema: comecou a fazer a seção do exosistema
    
    * icones do ecosistema
    
    * Ecosistema: finalizado o ecosistema
    
    * vários ajustes
    
    * change styles.css
    
    * criado a versão minificada do styles.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+
+const FlipCard = plugin(function ({ addUtilities }) {
+  addUtilities({
+    ".my-rotate-y-180": {
+      transform: "rotateY(180deg)",
+    },
+    ".preserve-3d": {
+      transformStyle: "preserve-3d",
+    },
+    ".perspective": {
+      perspective: "1000px",
+    },
+    ".backface-hidden": {
+      backfaceVisibility: "hidden",
+    },
+  });
+});
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
@@ -8,12 +27,26 @@ module.exports = {
         'roboto': ['"Roboto"', 'sans-serif']
       },
       colors: {
-        primary: "#ef2d89",
-        secondary: "#6c3d70",
-        third: "#ffde14",
-        fourth: "#ffcd1c"
+        primary: '#ef2d89',
+        secondary: '#6c3d70',
+        third: '#ffde14',
+        fourth: '#ffcd1c',
+        fifth: 'rgba(239,45,137,0.03)',
+        sixth: 'rgba(18,17,16,0.6)',
+        seventh: '#a3807e',
       },
+      backgroundImage: {
+        'services-image': "linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('./images/bg-services.jpeg')",
+        'ecosistema-image': "linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('./images/box.webp')",
+        'bg-depoimento': "url('./images/bg-depoimento.jpg')",
+      },
+      backgroundSize: {
+        '120%': '120%',
+        '150%': '150%',
+        '175%': '175%',
+        '200%': '200%',
+      }
     },
   },
-  plugins: [],
+  plugins: [FlipCard],
 }
